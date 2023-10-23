10aeb4ef9563d8a5aed0ee6afedbcac05ecd248d
Darryl <kwekleedarryl@gmail.com>
2023-10-23 11:44:00 +0800

    Updated with Unity Game Project
    Updated and Uploaded with the Unity Game Project File by Me

---
+++ 15_Darryl Kwek_Lab10/Assets/Scripts/Player.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+
+    private Vector2 screenBounds;
+    private float objectHeight;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        objectHeight = transform.GetComponent<MeshRenderer>().bounds.size.y / 2;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.95f, 3.95f), 0);
+
+    }
+
+
+    /*
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            SceneManager.LoadScene("LoseScene");
+        }
+    }
+    */
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Obstacle")
+        {
+            SceneManager.LoadScene("LoseScene");
+        }
+    }
+}
---
+++ 15_Darryl Kwek_Lab10/Assets/Scripts/Spawnerscript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Spawnerscript : MonoBehaviour
+{
+    //public GameObject SpawnObject;
+    float PositionY;
+
+    public GameObject[] myObjects;
+
+    public Text ScoreText;
+    public static float Score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObjects", 1, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ScoreText.text = "Score: " + Score;
+
+
+    }
+
+    void SpawnObjects()
+    {
+        PositionY = Random.Range(4, -4f);
+        int randomIndex = Random.Range(0, myObjects.Length);
+        this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
+        Instantiate(myObjects[randomIndex], transform.position, transform.rotation);
+    }
+}
